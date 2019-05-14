3e2979101303e7bf2884ed51bedc1da1a5d99111
AcerYue0 <leeyuchin2618005@gmail.com>
2019-05-14 21:42:21 +0800

    Probebility logic change.
    norelic become to 1, 1, 1, 2
    fake mape become to 1, 1, 1, 7
    real map become to 2, 1, 2, 0

--- DM Probability Simulator/src/Simulation.java
+++ DM Probability Simulator/src/Simulation.java
@@ -71,42 +71,33 @@ public class Simulation {
 	private void NextBlock(int fromX, int fromY, int toX, int toY) {
 		//no relic 1, 1, 1, 2 (right, forward, left, backward)
 		//fake map 1, 1, 1, 7 (right, forward, left, backward)
-		//real map 3, 2, 3, 0 (right, forward, left, backward)
+		//real map 2, 1, 2, 0 (right, forward, left, backward)
 		int k;
 		int NewX = toX;
 	    int NewY = toY;
 		switch(RelicState) {
 		case NoRelic:
-			k = rnd.nextInt(4);
-			//edge test
+			k = rnd.nextInt(5);
 			if(toX == bossEntranceX && toY == bossEntranceY) {
 				dungeon[bossEntranceX][bossEntranceY]++;
-		    } else if(toX == 0 && k == 0) {
+		    } else if(toX == 0 && k >= 0 && k <= 0) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toY == (dungeonY - 1) && k == 1) {
+		    } else if(toY == (dungeonY - 1) && k >= 1 && k <= 1) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toX == (dungeonX - 1) && k == 2) {
+		    } else if(toX == (dungeonX - 1) && k >= 2 && k <= 2) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toY == 0 && k == 3) {
+		    } else if(toY == 0 && k >= 3 && k <= 4) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } 
-			//try go to next room
-		    else {
-		        switch(k)
-		        {
-		            case 0:
-		                NewX--;
-		                break;
-		            case 1:
-		                NewY++;
-		                break;
-		            case 2:
-		                NewX++;
-		                break;
-		            case 3:
-		                NewY--;
-		                break;
-		        }
+		    } else {
+		    	if(inRange(k, 0, 0)) {
+	                NewX--;
+		    	} else if (inRange(k, 1, 1)) {
+	                NewY++;
+		    	} else if (inRange(k, 2, 2)) {
+	                NewX++;
+		    	} else if (inRange(k, 3, 4)) {
+	                NewY--;
+		    	}
 		        if(NewX == fromX && NewY == fromY) {
 		            NextBlock(fromX, fromY, toX, toY);
 		        } else {
@@ -116,25 +107,25 @@ public class Simulation {
 		    }
 			break;
 		case FakeMap:
-			k = rnd.nextInt(20);
+			k = rnd.nextInt(10);
 			if(toX == bossEntranceX && toY == bossEntranceY) {
 				dungeon[bossEntranceX][bossEntranceY]++;
-		    } else if(toX == 0 && k >= 0 && k <= 5) {
+		    } else if(toX == 0 && k >= 0 && k <= 0) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toY == (dungeonY - 1) && k >= 6 && k <= 8) {
+		    } else if(toY == (dungeonY - 1) && k >= 1 && k <= 1) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toX == (dungeonX - 1) && k >= 9 && k <= 14) {
+		    } else if(toX == (dungeonX - 1) && k >= 2 && k <= 2) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toY == 0 && k >= 15 && k <= 20) {
+		    } else if(toY == 0 && k >= 3 && k <= 9) {
 		        NextBlock(fromX, fromY, toX, toY);
 		    } else {
-		    	if(inRange(k, 0, 5)) {
+		    	if(inRange(k, 0, 0)) {
 	                NewX--;
-		    	} else if (inRange(k, 6, 8)) {
+		    	} else if (inRange(k, 1, 1)) {
 	                NewY++;
-		    	} else if (inRange(k, 9, 14)) {
+		    	} else if (inRange(k, 2, 2)) {
 	                NewX++;
-		    	} else if (inRange(k, 15, 20)) {
+		    	} else if (inRange(k, 3, 9)) {
 	                NewY--;
 		    	}
 		        if(NewX == fromX && NewY == fromY) {
@@ -146,27 +137,27 @@ public class Simulation {
 		    }
 			break;
 		case RealMap:
-			k = rnd.nextInt(8);
+			k = rnd.nextInt(5);
 			if(toX == bossEntranceX && toY == bossEntranceY) {
 				dungeon[bossEntranceX][bossEntranceY]++;
-		    } else if(toX == 0 && k >= 0 && k <= 2) {
+		    } else if(toX == 0 && k >= 0 && k <= 1) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toY == (dungeonY - 1) && k >= 3 && k <= 4) {
+		    } else if(toY == (dungeonY - 1) && k >= 2 && k <= 2) {
 		        NextBlock(fromX, fromY, toX, toY);
-		    } else if(toX == (dungeonX - 1) && k >= 5 && k <= 8) {
+		    } else if(toX == (dungeonX - 1) && k >= 3 && k <= 4) {
 		        NextBlock(fromX, fromY, toX, toY);
 		    } else {
-		    	if(inRange(k, 0, 2)) {
+		    	if(inRange(k, 0, 1)) {
 		    		if(fromX == (dungeonX - 1) && isBackwardingWithRealMap) {
 		    			isBackwardingWithRealMap = false;
 		    		}
 	                NewX--;
-		    	} else if (inRange(k, 3, 4)) {
+		    	} else if (inRange(k, 2, 2)) {
 		    		if(NewY == 0) {
 		    			isBackwardingWithRealMap = false;
 		    		}
 		    		NewY = isBackwardingWithRealMap ? NewY - 1 : NewY + 1;
-		    	} else if (inRange(k, 5, 8)) {
+		    	} else if (inRange(k, 3, 4)) {
 		    		if(fromX == 0 && isBackwardingWithRealMap) {
 		    			isBackwardingWithRealMap = false;
 		    		}
